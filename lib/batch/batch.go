7e263e598dfbabb7267033829a2c31ad4734f8ca
vital <vitalikgk@mail.ru>
2022-07-01 09:46:55 +0400

    test

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,10 +1,15 @@
 package batch
 
 import (
+	"fmt"
+	"sync"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type user struct {
+	//	m       sync.Mutex
 	ID int64
 }
 
@@ -14,5 +19,33 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	res = make([]user, 0, n)
+	g := new(errgroup.Group)
+	g.SetLimit(int(pool))
+	var m sync.Mutex
+	var i int64
+	//	var wg sync.WaitGroup
+
+	for i = 0; i < n; i++ {
+		index := i
+
+		g.Go(func() error {
+			user := getOne(index)
+
+			m.Lock()
+			defer m.Unlock()
+			fmt.Println("Go index = ", index)
+			res = append(res, user)
+
+			return nil
+		})
+	}
+	err := g.Wait()
+	if err != nil {
+		fmt.Println("Ошибка: ", err)
+	} else {
+		fmt.Println("All Done!", res)
+	}
+
+	return res
 }
